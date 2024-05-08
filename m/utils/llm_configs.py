1f2b392855cc9fd7b73f5ee19bfb46b9a70c5083
Pablo Schaffner <pablo@puntorigen.com>
2024-05-07 22:17:28 -0400

    - updated model specs

--- m/utils/llm_configs.py
+++ m/utils/llm_configs.py
@@ -62,13 +62,31 @@ llm_configs = {
         "minimum_gpu_required": None,
         "required_disk_space": None,
     },
-    "ollama/minstral": {
-        "context_window_tokens": 2048,
-        "max_output_tokens": 1024,
+    "ollama/mistral:instruct": {
+        "context_window_tokens": 32000,
+        "max_output_tokens": 8192,
         "expert_for": ["everything", "reasoning", "coding"],
         "local": True,
         "minimum_ram_required": 16,  # GB
         "minimum_gpu_required": False,
-        "required_disk_space": 40,  # GB
+        "required_disk_space": 4.1,  # GB
+    },
+    "ollama/llama3:instruct": {
+        "context_window_tokens": 8192,
+        "max_output_tokens": 2048,
+        "expert_for": ["everything", "reasoning", "coding"],
+        "local": True,
+        "minimum_ram_required": 16,  # GB
+        "minimum_gpu_required": False,
+        "required_disk_space": 4.7,  # GB
+    },
+    "ollama/phi3:instruct": {
+        "context_window_tokens": 128000,
+        "max_output_tokens": 2048,
+        "expert_for": ["coding", "python"],
+        "local": True,
+        "minimum_ram_required": 16,  # GB
+        "minimum_gpu_required": False,
+        "required_disk_space": 2.3,  # GB
     },
 }
