3fa2f8e87851636d24e8cb479eb9e769eff6624a
Pablo Schaffner <pablo@puntorigen.com>
2024-05-08 19:02:56 -0400

    -fixed localizer to store vars placeholders correctly on .po and .mo files

--- m/utils/localizer.py
+++ m/utils/localizer.py
@@ -1,4 +1,3 @@
-# localizer.py
 import os
 import polib
 import gettext
@@ -28,11 +27,38 @@ class Localizer:
         return translated_text
 
     def _(self, text, *args, **kwargs):
-        """Format the translated string with dynamic parameters, mimicking the gettext `_` function, keeping placeholders intact."""
-        translated_text = self.translate(text)
+        """Format the translated string with dynamic parameters, keeping placeholders intact."""
+        # Temporarily replace placeholders with unique identifiers
+        temp_text, unique_to_placeholder = self._replace_placeholders_with_unique(text, **kwargs)
+
+        # Translate the modified text
+        translated_temp_text = self.translate(temp_text)
+
+        # Restore original placeholders in the final translation
+        translated_text = self._restore_placeholders(translated_temp_text, unique_to_placeholder)
+
+        # Update .po file using the original text (with actual placeholders)
+        self.update_po_file(text, translated_text, self.target_lang, "Online" if self.online else "Offline")
+
         return translated_text.format(*args, **kwargs)
 
-    def update_po_file(self, text, translation, target_lang, method):
+    def _replace_placeholders_with_unique(self, text, **kwargs):
+        """Replace placeholders with unique identifiers."""
+        unique_to_placeholder = {}
+        for i, placeholder in enumerate(kwargs.keys()):
+            unique_key = f'_PLACEHOLDER_{i}_'
+            original_placeholder = f'{{{placeholder}}}'
+            text = text.replace(original_placeholder, unique_key)
+            unique_to_placeholder[unique_key] = original_placeholder
+        return text, unique_to_placeholder
+
+    def _restore_placeholders(self, text, unique_to_placeholder):
+        """Restore unique identifiers back to original placeholders."""
+        for unique_key, placeholder in unique_to_placeholder.items():
+            text = text.replace(unique_key, placeholder)
+        return text
+
+    def update_po_file(self, original_text, translated_text, target_lang, method):
         """Update the .po file with a new translation and recompile to .mo."""
         po_file_path = os.path.join(self.locale_path, target_lang, 'LC_MESSAGES', f'{self.domain}.po')
         mo_file_path = os.path.join(self.locale_path, target_lang, 'LC_MESSAGES', f'{self.domain}.mo')
@@ -59,18 +85,28 @@ class Localizer:
             }
 
         # Find or create the entry
-        entry = po.find(text)
+        entry = po.find(original_text)
         if entry is None:
-            entry = polib.POEntry(msgid=text, msgstr=translation)
+            entry = polib.POEntry(msgid=original_text, msgstr=translated_text)
             po.append(entry)
         else:
-            entry.msgstr = translation
+            entry.msgstr = translated_text
 
         # Add translation method as a comment
         entry.comment = f'Translated using {method} translation.'
+
+        # Remove entries with placeholders
+        po = self._remove_placeholder_entries(po)
 
         # Save the updated .po file
         po.save(po_file_path)
 
         # Compile to .mo file
         po.save_as_mofile(mo_file_path)
+
+    def _remove_placeholder_entries(self, po):
+        """Remove entries with _PLACEHOLDER_ strings."""
+        to_remove = [entry for entry in po if "_PLACEHOLDER_" in entry.msgid or "_PLACEHOLDER_" in entry.msgstr]
+        for entry in to_remove:
+            po.remove(entry)
+        return po
